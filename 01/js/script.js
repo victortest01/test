0f6bd620251b26ee441585cbc6cb4d965418f5ce
Victor Rudkovsky <vrudkovsky@vitasoftua.com>
2020-03-16 13:15:05 +0200

    01

---
+++ 01/js/script.js
@@ -0,0 +1,18 @@
+console.log('Hello world!!');
+console.log("Hey Guys!");
+console.log("2342");
+console.log('Hello ' + 'world!');
+// console.info('JavaScript');
+// document.getElementById('out').innerHTML = 'Hello';
+// document.getElementById('out').innerHTML = 2019;
+document.getElementById('out').innerHTML = '<b>2019</b>';
+
+// document.querySelector('h2').innerHTML = 5;
+document.querySelector('.header').innerHTML = 5;
+document.querySelector('#one').innerHTML = 777;
+
+let a = document.querySelector('#one');
+let b = document.querySelector('.header');
+
+a.innerHTML = 9999;
+b.innerHTML = "Lesson #1";
